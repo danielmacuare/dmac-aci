8f5b6466e2e6d1ef2aadc70839b4ae2ccf4d3d5c
Daniel Macuare <daniel280187@hotmail.com>
2026-04-06 15:07:22 +0200

    Adds Interface Profile and Port Selectors

--- tform/tenants/prod/1-access-policies.tf
+++ tform/tenants/prod/1-access-policies.tf
@@ -59,7 +59,7 @@ resource "aci_fabric_if_pol" "ten_gig_speed" {
 
 # Create Interface vPC Policy Groups
 
-resource "aci_leaf_access_bundle_policy_group" "ESXILab01_VPC" {
+resource "aci_leaf_access_bundle_policy_group" "esxilab01_VPC" {
   name                            = "ESXILab01_VPC"
   description = "${var.tform_managed} - Interface vPC Policy Group ESXILab01_VPC"
   lag_t       = "node" #node for vCP, link for single switches Port Channel
@@ -70,38 +70,93 @@ resource "aci_leaf_access_bundle_policy_group" "ESXILab01_VPC" {
   relation_infra_rs_h_if_pol    = aci_fabric_if_pol.ten_gig_speed.id
 }
 
+resource "aci_leaf_access_bundle_policy_group" "esxilab02_VPC" {
+  name                            = "ESXILab02_VPC"
+  description = "${var.tform_managed} - Interface vPC Policy Group ESXILab02_VPC"
+  lag_t       = "node" #node for vCP, link for single switches Port Channel
+  relation_infra_rs_cdp_if_pol  = aci_cdp_interface_policy.cdp_enable.id
+  relation_infra_rs_lldp_if_pol = aci_lldp_interface_policy.lldp_enable.id
+  relation_infra_rs_lacp_pol    = aci_lacp_policy.lacp_active.id
+  relation_infra_rs_att_ent_p   = aci_attachable_access_entity_profile.dmacprod_aaep.id
+  relation_infra_rs_h_if_pol    = aci_fabric_if_pol.ten_gig_speed.id
+}
 
-# Interface Profile (Por Selectors)
+resource "aci_leaf_access_bundle_policy_group" "esxilab03_VPC" {
+  name                            = "ESXILab03_VPC"
+  description = "${var.tform_managed} - Interface vPC Policy Group ESXILab03_VPC"
+  lag_t       = "node" #node for vCP, link for single switches Port Channel
+  relation_infra_rs_cdp_if_pol  = aci_cdp_interface_policy.cdp_enable.id
+  relation_infra_rs_lldp_if_pol = aci_lldp_interface_policy.lldp_enable.id
+  relation_infra_rs_lacp_pol    = aci_lacp_policy.lacp_active.id
+  relation_infra_rs_att_ent_p   = aci_attachable_access_entity_profile.dmacprod_aaep.id
+  relation_infra_rs_h_if_pol    = aci_fabric_if_pol.ten_gig_speed.id
+}
+
+
+# Interface Profile 
+resource "aci_leaf_interface_profile" "leaf101_102_ip" {
+  name = "Leaf101_102_IP"
+}
+
+# Port Selectors: Eth-1/1, Eth-1/2, Eth-1/3
+# esxi-lab-01 (Eth-1/1)
+resource "aci_access_port_selector" "esxilab01_port_1_1" {
+  leaf_interface_profile_dn      = aci_leaf_interface_profile.leaf101_102_ip.id
+  name                           = "eth1_1"
+  access_port_selector_type      = "range"
+  
+  relation_infra_rs_acc_base_grp = aci_leaf_access_bundle_policy_group.esxilab01_VPC.id
+}
+
+resource "aci_access_port_block" "esxilab01_blk" {
+  access_port_selector_dn = aci_access_port_selector.esxilab01_port_1_1.id
+  name                    = "blk1"
+  from_card               = "1"
+  from_port               = "1" # Defines Port 1/1
+  to_card                 = "1"
+  to_port                 = "1"
+}
+
+# esxi-lab-02 (Eth-1/2)
+resource "aci_access_port_selector" "esxilab02_port_1_2" {
+  leaf_interface_profile_dn      = aci_leaf_interface_profile.leaf101_102_ip.id
+  name                           = "eth1_2"
+  access_port_selector_type      = "range"
+  
+  relation_infra_rs_acc_base_grp = aci_leaf_access_bundle_policy_group.esxilab02_VPC.id
+}
+
+resource "aci_access_port_block" "esxilab02_blk" {
+  access_port_selector_dn = aci_access_port_selector.esxilab02_port_1_2.id
+  name                    = "blk1"
+  from_card               = "1"
+  from_port               = "2" # Defines Port 1/2
+  to_card                 = "1"
+  to_port                 = "2"
+}
+
+
+# esxi-lab-03 (Eth-1/3)
+resource "aci_access_port_selector" "esxilab03_port_1_3" {
+  leaf_interface_profile_dn      = aci_leaf_interface_profile.leaf101_102_ip.id
+  name                           = "eth1_3"
+  access_port_selector_type      = "range"
+  
+  relation_infra_rs_acc_base_grp = aci_leaf_access_bundle_policy_group.esxilab03_VPC.id
+}
+
+resource "aci_access_port_block" "esxilab03_blk" {
+  access_port_selector_dn = aci_access_port_selector.esxilab03_port_1_3.id
+  name                    = "blk1"
+  from_card               = "1"
+  from_port               = "3" # Defines Port 1/3
+  to_card                 = "1"
+  to_port                 = "3"
+}
+
 
 
 # Switch Profile
-#resource "aci_leaf_profile" "example" {
-  #name        = "Leaf101_102_SP"
-  #description  = "${var.tform_managed} - Switch Profile Leaf101_102_SP"
-  #leaf_selector {
-    #name                    = "leaf101"
-    #switch_association_type = "range" # ALL, range or ALL_IN_POD
-    #node_block {
-      #name  = "blk1"
-      #from_ = "101"
-      #to_   = "102"
-    #}
-    #node_block {
-      #name  = "blk2"
-      #from_ = "103"
-      #to_   = "104"
-    #}
-  #}
-  #leaf_selector {
-    #name                    = "leaf102"
-    #switch_association_type = "range"
-    #node_block {
-      #name  = "blk3"
-      #from_ = "105"
-      #to_   = "107"
-    #}
-  #}
-#}
 
 
 
