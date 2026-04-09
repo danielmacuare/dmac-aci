7f83873337fe1a2fe11f1fae3e0dd75edacaf54c
Daniel Macuare <daniel280187@hotmail.com>
2026-04-09 13:42:52 +0200

    Adds External EPG Definition and subnets

--- tform/tenants/prod/tenant-pols/locals.tf
+++ tform/tenants/prod/tenant-pols/locals.tf
@@ -10,18 +10,29 @@ locals {
     "topology/pod-1/protpaths-101-102/pathep-[ESXILab03_VPC]"
   ]
   
-  nsxt_interfaces = {
+  nsxt_border_leaves = {
     leaf101 = { 
       topology_path = "topology/pod-1/node-101"
       interface_path = "topology/pod-1/paths-101/pathep-[eth1/45]"
       ip   = "10.60.10.1/29"
+      peer_ip = "10.60.10.2"
       router_id = "1.1.1.101"
+      remote_asn = "65002"
+      received_prefixes = ["10.40.0.0/16", "10.50.0.0/16", "10.60.0.0/16"]
     }
     leaf102 = { 
       topology_path = "topology/pod-1/node-102"
       interface_path = "topology/pod-1/paths-102/pathep-[eth1/46]"
-      ip   = "10.60.10.2/29"
+      ip   = "10.60.10.3/29"
+      peer_ip = "10.60.10.4"
       router_id = "1.1.1.102"
+      remote_asn = "65003"
+      received_prefixes = ["10.40.0.0/16", "10.50.0.0/16", "10.60.0.0/16"]
     }
   }
+  # toset - Elimiantes all duplicates
+  # flatten - Creates a single list with all the elements matches in the loop
+  nsxt_received_prefixes = toset(flatten([
+    for node in local.nsxt_border_leaves : node.received_prefixes
+  ]))
 }
--- tform/tenants/prod/tenant-pols/nsxt.tf
+++ tform/tenants/prod/tenant-pols/nsxt.tf
@@ -29,7 +29,7 @@ resource "aci_logical_node_profile" "nsxt_l3out" {
 
 # Attach Logical node to Border Leaf101 and 102
 resource "aci_logical_node_to_fabric_node" "nsxt_nodes" {
-  for_each = local.nsxt_interfaces
+  for_each = local.nsxt_border_leaves
 
   logical_node_profile_dn = aci_logical_node_profile.nsxt_l3out.id
   
@@ -51,13 +51,50 @@ resource "aci_logical_interface_profile" "nsxt_l3out_intprof" {
 
 # Attach Paths to Border Leafs dynamically
 resource "aci_l3out_path_attachment" "nsxt_paths" {
-  for_each = local.nsxt_interfaces
+  for_each = local.nsxt_border_leaves
 
   logical_interface_profile_dn = aci_logical_interface_profile.nsxt_l3out_intprof.id
   
   target_dn                    = each.value.interface_path
   addr                         = each.value.ip
   
+  #L3 Routed sub interfaces are p2p connections so leaf101 and leaf102 can't share the same /29 segment.
+  # Since we are building a common L2 Domain for all BGP peers, we need to use an SVI here "ext-svi". 
   if_inst_t                    = "ext-svi"
   encap                        = "vlan-405"
+}
+
+################################################################
+# BGP Peer Connectivity Profile for NSX-T L3 Out
+################################################################
+
+resource "aci_bgp_peer_connectivity_profile" "nsxt_bgp_peer" {
+  for_each = local.nsxt_border_leaves
+
+  parent_dn = aci_l3out_path_attachment.nsxt_paths[each.key].id
+
+  addr      = each.value.peer_ip
+  as_number = each.value.remote_asn
+}
+
+################################################################
+# External EPG Container and subnets for NSX-T Workloads
+################################################################
+
+resource "aci_external_network_instance_profile" "nsxt_ext_epg" {
+  l3_outside_dn = aci_l3_outside.nsxt_l3out.id
+  name          = "NSXT_ExtEPG"
+  description   = "${var.tform_managed} - External EPG for NSX-T Workloads"
+}
+
+resource "aci_l3_ext_subnet" "nsxt_catch_all_subnet" {
+
+
+  for_each = local.nsxt_received_prefixes
+  external_network_instance_profile_dn = aci_external_network_instance_profile.nsxt_ext_epg.id
+  
+  ip                                   = each.value
+  
+  # CRITICAL: 'import-security' is required to allow Contracts to apply to this subnet
+  scope                                = ["import-security"] 
 }
