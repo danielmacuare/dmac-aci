7d2f02603bc52e4ac332355977e33247122e4c8a
Daniel Macuare <daniel280187@hotmail.com>
2026-04-09 11:36:46 +0200

    Adds Logical Node Profiles + Interface Profile + SVIs - P2

--- tform/tenants/prod/tenant-pols/nsxt.tf
+++ tform/tenants/prod/tenant-pols/nsxt.tf
@@ -1,5 +1,7 @@
 
-
+################################################################
+# Define NSX-T L3 Out
+################################################################
 resource "aci_l3_outside" "nsxt_l3out" {
   tenant_dn   = aci_tenant.dmacprod_tenant.id
   name        = "NSXT_L3Out"
@@ -13,4 +15,49 @@ resource "aci_l3_outside" "nsxt_l3out" {
   
   # 3. Enable BGP Route Export (Required to send routes to NSX-T)
   enforce_rtctrl = ["export"] 
+}
+
+################################################################
+# Define Logical Node Profiles for NSX-T L3 Out
+################################################################
+
+resource "aci_logical_node_profile" "nsxt_l3out" {
+  l3_outside_dn = aci_l3_outside.nsxt_l3out.id
+  name          = "NSXTL3OUT_NodeProfile"
+  description   = "${var.tform_managed} - Logical Node Profile for NSX-T L3 Out"
+}
+
+# Attach Logical node to Border Leaf101 and 102
+resource "aci_logical_node_to_fabric_node" "nsxt_nodes" {
+  for_each = local.nsxt_interfaces
+
+  logical_node_profile_dn = aci_logical_node_profile.nsxt_l3out.id
+  
+  tdn                     = each.value.topology_path
+  
+  rtr_id                  = each.value.router_id
+  rtr_id_loop_back        = "yes"
+}
+
+################################################################
+# Logical Interface Profiles & SVIs for NSX-T L3 Out
+################################################################
+
+resource "aci_logical_interface_profile" "nsxt_l3out_intprof" {
+  logical_node_profile_dn = aci_logical_node_profile.nsxt_l3out.id
+  name                    = "NSXTL3OUT_InterfaceProfile"
+  description             = "${var.tform_managed} - Logical Interface Profile for NSX-T L3 Out"
+}
+
+# Attach Paths to Border Leafs dynamically
+resource "aci_l3out_path_attachment" "nsxt_paths" {
+  for_each = local.nsxt_interfaces
+
+  logical_interface_profile_dn = aci_logical_interface_profile.nsxt_l3out_intprof.id
+  
+  target_dn                    = each.value.interface_path
+  addr                         = each.value.ip
+  
+  if_inst_t                    = "ext-svi"
+  encap                        = "vlan-405"
 }
